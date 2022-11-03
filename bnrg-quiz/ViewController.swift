3cea21742c42f8f7816918c7f182b72094b7415f
Tianbo <tianbo@getjerry.com>
2022-11-03 13:00:51 -0700

    Initial Commit

---
+++ bnrg-quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bnrg-quiz
+//
+//  Created by Tianbo Qiu on 11/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
